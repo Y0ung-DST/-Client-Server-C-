1ab89fcef2a8e8b7f54615806e408cc45c916504
Y0ung-DST <62240696+Y0ung-DST@users.noreply.github.com>
2021-07-28 14:20:44 +0100

    Update server.c

--- server.c
+++ server.c
@@ -1,14 +1,13 @@
 #define _WIN32_IE 0x0501
-
 #include <stdio.h>
 #include <winsock2.h>
 #include <windows.h>
 #include <shellapi.h>
-
 #define PORT 9002
 
 
-int main (int argc, char *argv[]) {
+int	main(int argc, char **argv)
+{
 	char buff[256];
 	int result = 0;
 	WSADATA wsa;
@@ -24,11 +23,8 @@ int main (int argc, char *argv[]) {
 		.szInfoTitle = "Server",
 		.dwInfoFlags = NIIF_INFO
 	};
-
 	Shell_NotifyIcon(NIM_ADD, &nid);
-
 	WSAStartup(0x22, &wsa);
-
 	listen_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	SOCKADDR_IN sock_addr = {
@@ -36,31 +32,22 @@ int main (int argc, char *argv[]) {
 		.sin_addr.s_addr = INADDR_ANY,
 		.sin_port        = htons(PORT)
 	};
-
 	bind(listen_sock, (SOCKADDR *)&sock_addr, sizeof(sock_addr));
-
-	while (1) {
+	while (1)
+	{
 		listen(listen_sock, SOMAXCONN);
-
 		data_sock = INVALID_SOCKET;
-
 		data_sock = accept(listen_sock, NULL, NULL);
-
-		while (1) {
+		while (1)
+		{
 			result = recv(data_sock, buff, sizeof(buff), 0);
-
 			if (result == SOCKET_ERROR || result == 0)
 				break;
-
 			buff[sizeof(buff) - 1] = '\0';
-
 			memcpy(nid.szInfo, buff, sizeof(buff));
-
 			Shell_NotifyIcon(NIM_MODIFY, &nid);
 		}
-
 		closesocket(data_sock);
 	}
-
-	return 0;
+	return (0);
 }
