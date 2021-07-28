14e1fbceaca5038aea11b413ef9041fcd859c992
Y0ung-DST <62240696+Y0ung-DST@users.noreply.github.com>
2021-07-28 14:17:37 +0100

    Update client.c

--- client.c
+++ client.c
@@ -1,36 +1,26 @@
 #include <stdio.h>
 #include <winsock2.h>
 #include <windows.h>
-
 #define SERVER_ADDR "192.168.1.1"
 //#define SERVER_ADDR "127.0.0.1" /* Forces IPv4 */
 #define SERVER_PORT 9002
 
-int main(int argc, char *argv[]) {
-	char buff [256];
+int	main(int ac, char **av) {
+	char	buff [256];
 	WSADATA wsa;
-
 	WSAStartup(0x22, &wsa);
-
 	Socket connect_sock = INVALID_SOCKET;
 	connect_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-
 	SOCKADDR_AIN sock_addr = {
-		   .sin_family        = AF_INET,
-		   .sin_addr.sin_addr = inet_addr(SERVER_ADDR)
-		   .sin_port          = htons(SERVER_PORT)
-
-	};
-
-	connect(connect_sock, (SOCKADDR *)&sock_addr, sizeof(sock_addr));
-
-	while (1) {
-		fgets(buff, sizeof(buff), stdin);
-
-		send(connect_sock, buff, strlen(buff) + 1, 0)
+		   .sin_family        = AF_INET;
+		   .sin_addr.sin_addr = inet_addr(SERVER_ADDR);
+		   .sin_port          = htons(SERVER_PORT);
 	}
-
-	return 0;
-	
-}	
+	connect(connect_sock, (SOCKADDR *)&sock_addr, sizeof(sock_addr));
+	while (1)
+	{
+		fgets(buff, sizeof(buff), stdin);
+		send(connect_sock, buff, strlen(buff) + 1, 0);
+	}
+	return (0);
+}
